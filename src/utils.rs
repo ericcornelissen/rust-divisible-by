913167933ef843975450aaf0bfc4e0f7e5a53430
Eric Cornelissen <ericornelissen@gmail.com>
2022-09-08 22:36:43 +0200

    Implement `last_digit`
    Implement the `last_digit` function, the reader is spared from having to
    view the TDD process on this one :)
    
    The result is an implementation closely matching what's seen in `lib.rs`
    with a few tests. First, the parameterized test is expanded with some
    additional cases capturing specific scenarios. Second, there's two
    property tests that I think cover what it means to return the last digit
    of a number:
    
    1. It returns single digit numbers.
    2. It returns some value that the original value, as a string,
      `ends_with()`.
    
    Together, these two properties _should_ guarantee the `last_digit`
    implementation always returns the last digit of a given number.

--- src/utils.rs
+++ src/utils.rs
@@ -1,17 +1,34 @@
 #![allow(dead_code)]
-pub fn last_digit(_n: u64) -> u64 {
-    todo!()
+pub fn last_digit(n: u64) -> u64 {
+    n.to_string().chars().last().unwrap().to_digit(10).unwrap() as u64
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    use proptest_attr_macro::proptest;
     use rstest::rstest;
 
     #[rstest]
     #[case(0, 0)]
-    fn returns_the_last_digit(#[case] n: u64, #[case] expected: u64) {
-        assert_eq!(last_digit(n), expected);
+    #[case(11, 1)]
+    #[case(123, 3)]
+    fn last_digit_returns_the_last_digit(#[case] n: u64, #[case] digit: u64) {
+        assert_eq!(last_digit(n), digit);
+    }
+
+    #[proptest]
+    fn last_digit_returns_a_single_digit(n: u64) {
+        assert!(matches!(
+            last_digit(n),
+            0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
+        ));
+    }
+
+    #[proptest]
+    fn last_digit_returns_something_n_ends_with(n: u64) {
+        let result = last_digit(n).to_string();
+        assert!(n.to_string().ends_with(&result[..]));
     }
 }
